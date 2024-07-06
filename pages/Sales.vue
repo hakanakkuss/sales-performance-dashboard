f6da5a6c34c7b942680618b80360dfa803f35451
hakanakkuss <hakannakkus6@gmail.com>
2024-07-06 14:16:48 +0300

    added delete modal to sales targets table

--- pages/Sales.vue
+++ pages/Sales.vue
@@ -1,28 +1,47 @@
 <script setup lang="ts">
 import Navigation from "~/pages/Navigation.vue";
-import { ref } from 'vue'
+import ConfirmationModal from "./components/DeleteModal.vue";
+import { ref } from 'vue';
 
-const { data } = await useFetch('/api/mockData')
+const { data } = await useFetch('/api/mockData');
+const salesTargets = ref(data.value.salesTargets);
+const showModal = ref(false);
+const itemToDelete = ref(null);
 
 const columnsMonthlySales = ref([
   { title: 'Name', key: 'name' },
   { title: 'Month', key: 'month' },
   { title: 'Sales', key: 'sales' }
-])
+]);
 
 const columnsTopSellingProducts = ref([
   { title: 'Product', key: 'product' },
-  { title: 'UnitsSold', key: 'unitsSold', sortable: true },
-])
+  { title: 'UnitsSold', key: 'unitsSold', sortable: true }
+]);
 
 const columnsSalesTargets = ref([
-  { id:'id', key: 'id' },
   { title: 'Month', key: 'month' },
   { title: 'Target', key: 'target', sortable: true },
   { title: 'Actual', key: 'actual', sortable: true },
+  { title: 'Actions', key: 'actions' }
+]);
 
-])
 
+
+const removeItem = () => {
+  salesTargets.value = salesTargets.value.filter(item => item.id !== itemToDelete.value);
+  closeModal();
+};
+
+const confirmDelete = (itemId) => {
+  itemToDelete.value = itemId;
+  showModal.value = true;
+};
+
+const closeModal = () => {
+  showModal.value = false;
+  itemToDelete.value = null;
+};
 </script>
 
 <template>
@@ -34,11 +53,27 @@ const columnsSalesTargets = ref([
 
   <div class="flex flex-col space-y-10 mt-20 ml-72 mr-72 justify-center h-screen min-w-screen">
     <h1 class="text-white text-2xl">Top Selling Products</h1>
-    <UTable class="text-white mt-20" :columns="columnsTopSellingProducts" :rows="data.topSellingProducts" />
+    <UTable class="text-white mt-20" :columns="columnsTopSellingProducts" :rows="data.topSellingProducts"> </UTable>
   </div>
 
   <div class="flex flex-col space-y-10 mt-20 ml-72 mr-72 justify-center h-screen min-w-screen">
     <h1 class="text-white text-2xl">Sales Targets</h1>
-    <UTable class="text-white mt-20" :columns="columnsSalesTargets" :rows="data.salesTargets" />
+    <UTable class="text-white mt-20" :columns="columnsSalesTargets" :rows="salesTargets">
+      <template #actions-data="{ row }">
+        <button @click="confirmDelete(row.id)">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" fill-rule="evenodd" d="M16.5 4.478v.227a49 49 0 0 1 3.878.512a.75.75 0 1 1-.256 1.478l-.209-.035l-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A49 49 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a53 53 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951m-6.136-1.452a51 51 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a50 50 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452m-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058z" clip-rule="evenodd"></path>
+          </svg>
+        </button>
+      </template>
+    </UTable>
   </div>
+
+  <!-- Modal Bileşeni -->
+  <ConfirmationModal
+      v-if="showModal"
+      :show="showModal"
+      :onConfirm="removeItem"
+      :onCancel="closeModal"
+  />
 </template>
---
+++ pages/components/DeleteModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="bg-menu-text p-6 rounded-lg shadow-md">
+      <h2 class="text-xl font-sans mb-4">Are you sure delete this user?</h2>
+      <div class="flex justify-center space-x-4">
+        <button @click="onCancel" class="bg-gray-300 text-black px-4 py-2 rounded">Cancel</button>
+        <button @click="onConfirm" class="bg-red-300 text-white px-4 py-2 rounded">Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  show: Boolean,
+  onConfirm: Function,
+  onCancel: Function
+});
+</script>
