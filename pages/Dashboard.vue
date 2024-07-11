2149bdedca5cf48b7ef69d0d0f5ad8d1b48c48d0
hakanakkuss <hakannakkus6@gmail.com>
2024-07-11 19:29:46 +0300

    added vue3-grid-layout and used in dashboard page

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -4,31 +4,71 @@ import PieChart from "~/pages/charts/PieChart.vue";
 import LineChart from "~/pages/charts/LineChart.vue";
 import BarChart from "~/pages/charts/BarChart.vue";
 import StackedLineChart from "~/pages/charts/StackedLineChart.vue";
+import { ref, onMounted } from "vue";
+import { GridLayout, GridItem } from 'vue3-grid-layout';
+
+const draggable = ref(true);
+const resizable = ref(true);
+
+const layout = ref([
+  { x: 0, y: 0, w: 6, h: 4, i: "StackedLineChart", static: false },
+  { x: 6, y: 0, w: 6, h: 4, i: "BarChart", static: false },
+  { x: 0, y: 6, w: 6, h: 4, i: "PieChart", static: false },
+  // { x: 6, y: 4, w: 6, h: 4, i: "LineChart", static: false },
+]);
+
+const getComponent = (name: string) => {
+  switch (name) {
+    case "StackedLineChart":
+      return StackedLineChart;
+    case "BarChart":
+      return BarChart;
+    case "PieChart":
+      return PieChart;
+    case "LineChart":
+      return LineChart;
+    default:
+      return null;
+  }
+};
+
+onMounted(() => {
+  getComponent();
+});
+
 
 </script>
 
 <template>
-  <div>
+  <div class="min-h-screen">
     <Navigation />
-  </div>
-  <div class="flex justify-center mx-auto py-6 px-4">
-    <div class="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2">
-      <div class=" p-6 rounded-lg ">
-        <StackedLineChart />
-      </div>
-      <div class=" p-6 rounded-lg">
-        <BarChart />
-      </div>
-      <div class=" p-6 rounded-lg mt-32">
-        <PieChart />
-      </div>
-      <div class="p-6 rounded-lg shadow-md">
-        <LineChart />
-      </div>
+    <div class="min-h-screen">
+      <GridLayout
+          :layout.sync="layout"
+          :col-num="12"
+          :row-height="90"
+          :is-draggable="draggable"
+          :is-resizable="resizable"
+          :vertical-compact="false"
+          :use-css-transforms="true"
+      >
+        <GridItem
+            v-for="item in layout"
+            :key="item.i"
+            :x="item.x"
+            :y="item.y"
+            :w="item.w"
+            :h="item.h"
+            :i="item.i"
+        >
+          <div class="shadow-md p-6 rounded-lg h-full">
+            <component :is="getComponent(item.i)" />
+          </div>
+        </GridItem>
+      </GridLayout>
     </div>
   </div>
-
-
-
 </template>
+
+
 
